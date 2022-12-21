14d8893f88088ced97631117c5c8af14207b7333
techpassel <techpasselorg@gmail.com>
2022-12-22 01:39:50 +0530

    Completed till Local Video View section

--- frontend/src/components/video-section/child-components/local-video-view/LocalVideoView.js
+++ frontend/src/components/video-section/child-components/local-video-view/LocalVideoView.js
@@ -2,21 +2,6 @@ import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './LocalVideoView.scss'
 
-const styles = {
-    videoContainer: {
-        width: '150px',
-        height: '150px',
-        borderRadius: '8px',
-        position: 'absolute',
-        top: '5%',
-        right: '5%'
-    },
-    videoElement: {
-        width: '100%',
-        height: '100%'
-    }
-};
-
 const LocalVideoView = () => {
     const { localStream } = useSelector((state) => state.call)
     const localVideoRef = useRef();
@@ -33,8 +18,8 @@ const LocalVideoView = () => {
     }, [localStream]);
 
     return (
-        <div style={styles.videoContainer} className="localVideoContainer">
-            <video style={styles.videoElement} ref={localVideoRef} autoPlay muted />
+        <div className="localVideoContainer">
+            <video className='localVideoElement' ref={localVideoRef} autoPlay muted />
         </div>
     );
 };
